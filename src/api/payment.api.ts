27794af700e511f1a179496d14aa81659962249f
sontransonn <sontransonn@gmail.com>
2025-01-17 10:20:57 +0700

    fix

--- src/api/payment.api.ts
+++ src/api/payment.api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/v1/payment',
+    baseURL: 'https://eduhub-be-sr86.onrender.com/v1/payment',
     withCredentials: true,
 });
 
